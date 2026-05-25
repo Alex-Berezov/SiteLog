d24d8c925ad6a47b254d68de90513a5d6d7722e1
Alex-Berezov <aleber197@gmail.com>
2026-05-25 16:27:26 +0400

    chore(db): add initial prisma migration

---
+++ server/prisma/migrations/20260525122717_init/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE "WorkType" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "WorkType_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "WorkLog" (
+    "id" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "workTypeId" TEXT NOT NULL,
+    "volume" DOUBLE PRECISION NOT NULL,
+    "unit" TEXT NOT NULL,
+    "workerName" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "WorkLog_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "WorkType_name_key" ON "WorkType"("name");
+
+-- AddForeignKey
+ALTER TABLE "WorkLog" ADD CONSTRAINT "WorkLog_workTypeId_fkey" FOREIGN KEY ("workTypeId") REFERENCES "WorkType"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
